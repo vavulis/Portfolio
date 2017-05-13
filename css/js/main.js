a28e07c49a0f46d4815107a0368a88997e45a866
Alex <Alex>
2017-05-13 13:25:46 +0300

    sdelal stranicy career

---
+++ css/js/main.js
@@ -0,0 +1,12 @@
+(function($){
+    $(function(){
+        $(".about-me__description-content").niceScroll({
+            cursorcolor: "#9DA2A8 ",
+            cursorborderradius: "5px",
+            railpadding: { top: 0, right: 0, left: 0, bottom: 0 },
+            autohidemode: false,
+            cursorminheight: 32
+        });
+    });
+})(jQuery);
+
